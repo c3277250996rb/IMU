498c2cba791e4ee6100fe8471f868005b24cb2fd
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2024-12-07 19:29:26 +0800

    2024/12/07 周六 19:29:16.38

---
+++ 分支/1207_优化/main/main_balance.m
@@ -0,0 +1,202 @@
+tic; % 开始计时
+addpath(genpath('data'));
+addpath(genpath('main'));
+addpath(genpath('src'));
+
+% 控制流程 变量 初始化
+
+    N = get_public_N() + 1;
+    time = zeros(N);
+    all_data = zeros(N,6);
+    x = zeros(N);
+
+    % 最大重试次数
+    maxRetries = 2;
+    retryCount = 0;
+
+    is_use_uart = get_is_use_uart();
+    input_is_use_rotate_accel = get_is_use_rotate_accel();
+
+    %
+
+    index_while = 1;
+    index_move_start = 0;
+    index_move_finish = 0;
+
+    % 运动标志 (default: 0)
+    % 0: 静止
+    % 1: 运动
+    flag_movement = 0;
+
+    % 计算标志 (default: 0)
+    % 0: 关闭
+    % 1: 开启
+    flag_calc = 0;
+    flag_calc_times = 0;
+
+    % 状态标志 (default: 0)
+    % 0: 静止
+    % 1: 震动
+    % 2: 往返
+    % 3: 移动
+    flag_status_number = 0;
+    flag_status_string = '初始化';
+
+    scan_begin_index = 50;
+    scan_move_avg_size = 50;
+    scan_stop_avg_size = 50;
+
+    mean_is_move = 0;
+    mean_is_stop = 0;
+    mean_before_move = 0;
+    % mean_after_move = 0;
+
+    count_stop_times = 0;
+    judge_stop_times = get_judge_stop_times();
+
+    threshold_is_move = 200;
+    threshold_is_stop = 400;
+%
+
+
+% 使用串口则打开
+if is_use_uart
+    % 打开串口
+    close_uart();
+    s = open_uart(get_com_number());
+
+% 不用串口则读取文件
+else
+    all_data = readtable(public_csv());
+
+    x = all_data.x;
+end
+
+
+% 死循环
+while true
+
+% (实时数据)
+    if is_use_uart
+        % 读取串口
+        while retryCount < maxRetries
+            try
+                all_data(index_while,:) = com2data(s);
+                break;
+            catch ME
+                % 捕获错误并显示错误信息
+                retryCount = retryCount + 1;
+                fprintf('执行失败: %s\n', ME.message);
+                fprintf('重试第 %d 次...\n', retryCount);
+                % 检查是否达到最大重试次数
+                if retryCount >= maxRetries
+                    fprintf('达到最大重试次数，停止执行。\n');
+                end
+            end
+        end
+        x(index_while) = all_data(index_while, 1);
+
+        % 初始化信息
+        if index_while < 2*scan_move_avg_size
+            disp(['[', flag_status_string, '][', num2str(index_while), ']: ', num2str(x(index_while))]);
+        end
+        if index_while == 100
+            disp('[初始化][完成]');
+        end
+    end
+
+% 运动识别
+    if index_while > 2*scan_move_avg_size
+        % 检测: 静止 -> 运动  (获取起始索引: index_move_start)
+            if flag_movement == 0
+                mean_is_move = mean( x(index_while - scan_move_avg_size : index_while) );
+
+                % abs(当前 - 均值) > 运动阈值 吗
+                if abs( x(index_while) - mean_is_move ) > threshold_is_move
+                    flag_movement = 1; % 标记运动
+                    % index_move_start = index_while; % 更新起始索引
+                    index_move_start = index_while - scan_move_avg_size; % 更新起始索引
+                    mean_before_move = mean( x(index_while - 2*scan_move_avg_size : index_while - scan_move_avg_size) ); % 更新零飘
+                end
+            end
+        %
+
+        % 检测: 运动 -> 静止  (获取结束索引: index_move_finish)
+            if flag_movement == 1
+                mean_is_stop = mean( x(index_while-scan_stop_avg_size : index_while) );
+
+                % abs(当前 - 均值) < 静止阈值 吗
+                if abs( mean_is_stop - x(index_while) ) < threshold_is_stop
+                    count_stop_times = count_stop_times + 1; % 静止次数累加
+
+                    if count_stop_times >= judge_stop_times
+                        count_stop_times = 0; % 清除计数
+                        flag_movement = 0; % 标记静止
+                        index_move_finish = index_while; % 更新结束索引
+                        flag_calc = 1; % 开启计算
+                    end
+
+                end
+
+            end
+        %
+
+        % 计算 & 状态
+            if flag_calc == 1
+                % 是否旋转
+                    if input_is_use_rotate_accel
+                        input_data = zeros(length(x));
+                        % disp(all_data(1:6,1:6));
+                        temp_data = rotateAcceleration(all_data);
+                        input_data = temp_data(:,1); % x列
+                    else
+                        input_data = x;
+                    end
+                %
+
+                % 计算
+                    input_start = index_move_start;
+                    input_finish= index_move_finish;
+                    input_mean  = mean_before_move;
+                    input_judge_stop_times = judge_stop_times;
+                    input_is_use_uart = is_use_uart;
+                    output_map     = ukf_balance(input_data, input_start, input_finish, input_mean, input_judge_stop_times);
+                    
+                    output_status = output_map('output_status');
+                    output_tail_d = output_map('output_tail_d');
+                %
+
+                % 打印状态
+                    flag_status_number = output_status;
+                    flag_status_string = flag_status_num2str(flag_status_number);
+                    disp(['[时间]: ', char(datetime('now'))]);
+                    flag_calc_times = flag_calc_times + 1; % 次数累加
+                    disp(['[状态][', num2str(flag_calc_times), ']: ', flag_status_string]);
+                    disp(['[距离][', num2str(flag_calc_times), ']: ', num2str(output_tail_d)]);
+                    disp(' '); % 空白换行
+                % 
+
+                % 清理
+                    index_move_start = 0;
+                    index_move_finish = 0;
+                    flag_calc = 0;
+                    flag_movement = 0;
+                %
+            end
+        %
+    end
+
+% (退出循环)
+    index_while = index_while + 1;
+    if index_while == (N - 1)
+        if is_use_uart == 0
+            break;
+        end
+    end
+
+end
+
+
+
+elapsedTime = toc; % 结束计时并获取所用时间
+fprintf('代码块执行时间为 %.4f 秒\n', elapsedTime);
---
+++ 分支/1207_优化/src/scripts/ukf/2nd_balance/ukf_balance.m
@@ -0,0 +1,177 @@
+function output_map = ukf_balance(input_data, input_start, input_finish, input_mean, input_judge_stop_times)
+
+    % 初始化
+        addpath(genpath('data'));
+        addpath(genpath('main'));
+        addpath(genpath('src'));
+
+        output_map = containers.Map();
+
+        input_is_use_uart = get_is_use_uart();
+        input_is_use_rotate_accel = get_is_use_rotate_accel();
+        
+        a = 16.384;
+        N = input_finish - input_start;
+        T = 0.039;
+
+
+        % 使用 旋转 零飘
+        if input_is_use_rotate_accel
+            accelerate = input_data(input_start : input_finish) - mean(input_data(1:30));
+            accelerate = accelerate ./ a ./ 10;
+
+        % 不 使用 旋转 零飘
+        else
+            accelerate = input_data(input_start : input_finish) - input_mean;
+            accelerate = accelerate ./ a ./ 10;
+
+        end
+
+        % 绘图加速度
+            figure
+            hold on ;box on;
+            plot(accelerate ./ 5000 .* 24 ./ 4 .* a .* 10);
+        %
+
+        % UKF参数初始化
+            % Q：过程噪声的协方差，用于模拟系统中的随机扰动。
+            Q = 1;
+            % R：观测噪声的协方差，表示测量过程中的随机误差。
+            R = 1;
+            % L：状态变量的维度。
+            L = 2;    %?个变量
+            % alpha、kalpha、belta：UKF的超参数，用于调整Sigma点的生成和权重。
+            alpha=1;
+            kalpha=0;
+            belta=2;
+            % ramda：UKF的扩展参数，影响Sigma点的散布程度。
+            ramda=3-L;
+
+            for j=1:2*L+1
+                Wm(j)=1/(2*(L+ramda));
+                Wc(j)=1/(2*(L+ramda));
+            end
+            % Wm(1)=ramda/(L+ramda);%权值Wm的初值需要另外定
+            % Wc(1)=ramda/(L+ramda)+1-alpha^2+belta;%权值Wc的初值需要另外定
+            s = zeros(N);
+            Z = accelerate;
+            Xukf = zeros(L,N);
+            covariation_finally=eye(L);%协方差阵初始化
+        %
+    %
+
+
+%%% V 速度处理
+        % V 速度 计算
+            % a -> v (UKF)
+                [Xukf, Z] = my_ukf(  N, T, L , ...
+                                alpha, kalpha, belta, ramda, ...
+                                Wm, Wc, ...
+                                s, Z, Xukf, covariation_finally);
+            %
+
+            % a -> v (直接积分)
+                Zv = zeros(N);
+                % disp(Z);
+                for i=2:N
+                    Zv(i) = Zv(i - 1) + (Z(i - 1) + Z(i)) /2 *T;
+                end
+            %
+        %
+
+        % V 速度 画图 (UKF 原始)
+            hold on ;box on;
+            t=(0:1:N-1);
+            plot(t,Xukf(2,:));
+            original_v_Xukf = Xukf;
+        %
+
+        % V 速度 补偿
+            input_tail_size = input_judge_stop_times - 5;
+            input_near_size = 4;
+            input_accelerate = Z;
+            input_velocity = Xukf(2,:);
+            % 求 I 点: 求 a 和 v 的最后一个交点 (不要 a 和 v 低于一定阈值的点)
+            output_I_index = I_av_intersection(input_accelerate, input_velocity, input_judge_stop_times);
+            disp(['output_I_index: ', num2str(output_I_index)]);
+            % 求 II 点: 从 I 点往前第一个 a 的零点 (不要 near_size 附近的点)
+            output_velocity = velocity_compensation(output_I_index, input_tail_size, input_near_size, input_accelerate, input_velocity);
+            Xukf(2,:) = output_velocity;
+        %
+
+        % V 速度 画图 (UKF 补偿)
+            plot(t,Xukf(2,:));
+        %
+%%%
+
+
+%%% D 距离处理
+        % D 距离 计算
+            % v -> d (UKF)
+                distance = zeros(L, N);
+                [distance, original_v_Xukf] = my_ukf(  N, T, L , ...
+                                    alpha, kalpha, belta, ramda, ...
+                                    Wm, Wc, ...
+                                    s, original_v_Xukf, distance, covariation_finally);
+            %
+
+            % v -> d (直接积分) (原始 UKF)
+                Zd = zeros(N);
+                for i=2:N
+                    Zd(i) = Zd(i - 1) + (original_v_Xukf(2,i) + original_v_Xukf(2,i-1)) /2 *T;
+                    % Zd(i) = Zd(i - 1) + (Zv(i) + Zv(i-1)) /2 *T;
+                end
+            %
+
+        %
+        
+        % D 距离 画图 (直接积分 原始)
+            plot(t, Zd);
+        %
+        
+        % D 距离 补偿
+            Zd = zeros(N);
+            for i=2:N
+                Zd(i) = Zd(i - 1) + (Xukf(2,i) + Xukf(2,i-1)) /2 *T;
+                % Zd(i) = Zd(i - 1) + (Zv(i) + Zv(i-1)) /2 *T;
+            end
+        %
+        
+        % D 距离 画图 (直接积分 补偿)
+            plot(t, Zd);
+        %
+%%%
+
+
+        % 画图 标注
+            legend('a', 'v', 'v', 'd', 'd');
+
+            % 强制刷新图形窗口，立即显示图形
+            drawnow;
+        %
+        
+        % Status 状态 判断
+            input_accelerate = zeros(N);
+            input_velocity = zeros(N);
+            input_distance = zeros(N);
+
+            input_tail_size = 30;
+            input_accelerate = accelerate ./ 5000 .* 24 ./ 4 .* a .* 10;
+            input_original_v_Xukf = original_v_Xukf(2,:);
+            input_velocity = Xukf(2,:);
+            input_distance = Zd(1:N);
+            input_all_size = N;
+            output_status = status_judgement(input_all_size, input_tail_size, input_accelerate, input_original_v_Xukf, input_velocity, input_distance);
+        %
+
+        % Map 赋值
+            % 状态
+            output_map('output_status') = output_status;
+            % 末端d
+            index_compensated_v_last_zero = (zero_judgement(Xukf(2,:)));
+            i_c_v_l_z = index_compensated_v_last_zero(end);
+            output_map('output_tail_d') = mean( Zd( i_c_v_l_z-5:min(i_c_v_l_z+5,end) , 1) );
+        %
+    %
+end
+        
