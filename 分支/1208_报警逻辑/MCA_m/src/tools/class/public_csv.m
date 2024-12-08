15f8016e172ef2ce184137836b7e426a01d9649d
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2024-12-08 19:19:06 +0800

    2024/12/08 周日 19:18:46.83

---
+++ 分支/1208_报警逻辑/MCA_m/src/tools/class/public_csv.m
@@ -0,0 +1,43 @@
+function output_csv = public_csv()
+
+    ext = '.csv';
+    folder_1st = 'data\';
+    if get_is_use_uart()
+        folder_2nd = 'common\';
+    else
+        folder_2nd = 'storage\';
+    end
+
+    
+    % output_csv = ['6axis_data', ext];
+
+    % output_csv = [folder_1st, folder_2nd, 'return_fast_3cm', ext];
+    % output_csv = [folder_1st, folder_2nd, 'return_fast_20cm', ext];
+
+    % output_csv = [folder_1st, folder_2nd, 'return_slow_3cm', ext];
+    % output_csv = [folder_1st, folder_2nd, 'return_slow_20cm', ext];
+
+    % output_csv = [folder_1st, folder_2nd, 'shake_1cm', ext];
+    % output_csv = [folder_1st, folder_2nd, 'shake_2mm', ext];
+    output_csv = [folder_1st, folder_2nd, 'shake_double', ext];
+
+    % output_csv = [folder_1st, folder_2nd, 'single_fast_3cm', ext];
+    % output_csv = [folder_1st, folder_2nd, 'single_fast_20cm', ext];
+
+    % output_csv = [folder_1st, folder_2nd, 'single_slow_3cm', ext];
+    % output_csv = [folder_1st, folder_2nd, 'single_slow_20cm', ext];
+
+    % output_csv = [folder_1st, folder_2nd, '484', ext];
+
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_0.8m_return_2', ext];
+
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_0.8m_single_1', ext];
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_0.8m_single_2', ext];
+
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_2m_single_1', ext];
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_2m_single_2', ext];
+    % output_csv = [folder_1st, folder_2nd, '0.8mps_2m_single_3', ext];
+
+
+    disp(['[当前使用]: ', output_csv]);
+end
