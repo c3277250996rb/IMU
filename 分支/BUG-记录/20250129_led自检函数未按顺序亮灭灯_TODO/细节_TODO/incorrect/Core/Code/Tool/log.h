dabde659464d2129a6db6bc59bbc26faf8b83986
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-01-29 12:47:17 +0800

    2025/01/29 周三 12:47:13.64

---
+++ 分支/BUG-记录/20250129_led自检函数未按顺序亮灭灯_TODO/细节_TODO/incorrect/Core/Code/Tool/log.h
@@ -0,0 +1,3 @@
+#include "code.h"
+
+#define LOG(my_string, my_int) {printf("[%s][%d]\r\n", my_string, my_int);}
---
+++ 分支/BUG-记录/20250129_led自检函数未按顺序亮灭灯_TODO/错误/Core/Code/Senser/my_senser.h
@@ -0,0 +1,7 @@
+#ifndef __MY_SENSER_H
+#define __MY_SENSER_H
+
+
+
+#endif // !__MY_SENSER_H    
+
