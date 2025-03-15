d2f5ee16896596b84dea70fa8d8dceb3d09122b2
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-03-15 17:50:49 +0800

    2025/03/15 周六 17:50:44.10

---
+++ 分支/2025/0315_②_fatfs读写函数_备份/stm32f103rct6/Core/Code/Main/code.c
@@ -0,0 +1,44 @@
+#include "code.h"
+
+void CODE_MAIN(){
+    CODE_INIT();
+    CODE_SELF_TEST();
+    CODE_WHILE();
+}
+
+void CODE_INIT(){
+    DWT_Delay_Init();  // init DWT
+    Open_UART1_Receive_IT();  // init UART1
+    // MPU9250_Init();  // init IMU
+    // QMC5883_Init();  // init Mag
+    // LSM6DS3_Init();  // init Accel & Gyro
+    SPI_APP_Init(SPI_Class_1);
+}
+
+void CODE_SELF_TEST(){
+    // while(1){
+        my_w25q64_self_test();
+        my_test();
+        // my_gpio_self_test();
+        // my_led_self_test();
+        // my_usart_self_test();
+        // my_mpu9250_self_test();
+        // my_qmc5883_self_test();
+        // my_lsm6ds3_self_test();
+        // quaternion_self_test();
+        // muti_printf_self_test();
+    // }
+}
+
+void CODE_WHILE(){
+    int i1;
+
+    while(1){
+        BLUE_Toggle();
+        delay_ms(1000);
+    }
+
+}
+
+
+
