686eed8aba3f81670f588bb1d27e299356493022
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-03-27 11:51:54 +0800

    .

---
+++ 分支/2025/0328_③_在EIDE里面编译数组会出bug_MDK里就不会_😡😡/stm32f103rct6/Core/Code/Tool/quaternion.h
@@ -0,0 +1,38 @@
+#ifndef __QUATERNION_H
+#define __QUATERNION_H
+
+#include "my_qmc5883.h"
+#include "my_lsm6ds3.h"
+#include "my_fatfs.h"
+#include "atk_delay.h"
+
+#define Kp 4.5
+#define Ki 1.0
+
+#define QUATRE_LSB_ACCEL (float) \
+    1.0  /1000000  *61  *16384
+
+#define QUATER_LSB_GYRO (float) \
+    1.0  /1000000  *3500  *0.075
+
+#define QUATER_LSB_MAG (float) \
+    1.0  *1000  /6  /3000  
+
+extern float euler_angle[3];
+extern float buffer_9axes[9];
+
+extern float DM_angle_record[10][5];
+extern uint8_t Q_current_index;
+extern uint8_t is_Q_stop;
+enum
+{
+    enum_Stop = 0,
+    enum_Ready,
+    eunm_Continuous
+};
+
+void imu_convert_9axes(void);
+void imu_get_euler(void);
+
+
+#endif
