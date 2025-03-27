5d23c705bc8baa0dd383ba6c3c04f3e49cc91dca
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-03-27 11:37:48 +0800

    .

---
+++ 分支/2025/0328_②_数组有bug_😡😡/stm32f103rct6/Core/Code/System/fatfs/my_fatfs.h
@@ -0,0 +1,29 @@
+#ifndef __MY_FATFS_H
+#define __MY_FATFS_H
+
+#include "code.h"
+
+#include "ff.h"
+
+typedef struct __FATFS_Result_Struct
+{
+    int File_Size;
+}FATFS_Result_Struct;
+
+extern FATFS_Result_Struct FATFS_Result_default;
+extern long FATFS_Index_Current;
+
+FATFS_Result_Struct FATFS_Read(char* path);
+void FATFS_Write(char* path, char* write_buffer, int write_position);
+void FATFS_Delete(char* path);
+
+void FATFS_Init(void);
+void FATFS_Deinit(void);
+void FATFS_Scan(void);
+void FATFS_Muti_Test(void);
+FATFS_Result_Struct FATFS_Index_Check(void);
+
+FRESULT get_last_line_from_file(const char *ipath, char *ibuffer, size_t ibuffer_size);
+
+
+#endif
