6b00d52a15798a8e10ae1a0ecf703285e12f3449
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-04-01 17:19:26 +0800

    .

---
+++ 2025/0330_①_l151成长备份/04_04_i2c改版正点原子__😡🌚🥶/Core/Code/Main/code.h
@@ -0,0 +1,34 @@
+#ifndef __CODE_H
+#define __CODE_H
+
+    #include "main.h"
+    
+    
+
+    #include "my_gpio.h"
+    // #include "my_led.h"
+    #include "my_usart.h"
+    // #include "my_qmc5883.h"
+    // #include "my_lsm6ds3.h"
+    // #include "my_spi.h"
+    // #include "my_w25q64.h"
+    // #include "my_fatfs.h"
+
+    // #include "quaternion.h"
+    // #include "multi_printf.h"
+    // #include "convert_string.h"
+
+    // #include "atk_malloc.h"
+
+	#include "test.h"
+
+    void CODE_MAIN();
+    void CODE_INIT();
+    void CODE_SELF_TEST();
+    void CODE_WHILE();
+
+#endif // !__CODE_H
+
+
+
+
