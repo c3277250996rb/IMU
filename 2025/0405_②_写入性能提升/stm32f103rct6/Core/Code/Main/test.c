225f78dbf3098c55160f01d5e9bd31ec3e96a0e9
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-04-05 16:21:22 +0800

    .

---
+++ 2025/0405_②_写入性能提升/stm32f103rct6/Core/Code/Main/test.c
@@ -0,0 +1,18 @@
+#include "test.h"
+
+#if 32703
+
+int my_test(){
+    FATFS_Index_Check(1);
+    is_Q_stop = enum_Ready;
+    while(is_Q_stop != enum_Stop){
+        // 2.一组采?次
+        quaternion_self_test();
+    }
+    delay_ms(100);
+}
+
+#endif
+
+
+
---
+++ 2025/0405_②_写入性能提升/stm32f103rct6/Core/Code/Tool/data_management.h
@@ -0,0 +1,30 @@
+#ifndef __DATA_MANAGEMENT_H
+#define __DATA_MANAGEMENT_H
+
+#include "code.h"
+#include "ff.h"
+
+#define First_Cache_Than_Write
+extern int Cache_Max_Row;
+#define Info_Column 5
+
+
+typedef struct __DM_Result_Struct
+{
+    uint8_t Status;
+}DM_Result_Struct;
+
+extern DM_Result_Struct DM_Result_default;
+
+// app
+void DM_Write(float *input_Array, uint8_t previous_index);
+void set_DM_row(int input_row);
+// driver
+
+
+//
+int get_row(float arr[][5]);
+int get_column(float arr[][5]);
+
+
+#endif  // __DATA_MANAGEMENT_H
