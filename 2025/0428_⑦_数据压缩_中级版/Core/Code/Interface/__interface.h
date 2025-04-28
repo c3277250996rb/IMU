2300e486521ef64c0d00c5dd6c669efc678f9ccc
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-04-28 17:15:59 +0800

    .

---
+++ 2025/0428_⑦_数据压缩_中级版/Core/Code/Interface/__interface.h
@@ -0,0 +1,12 @@
+#ifndef __INTERFACE_H
+#define __INTERFACE_H
+
+    #include "main.h"
+
+    #include <stdbool.h>
+    #include <stdio.h>
+    #include <string.h>
+    #include <math.h>
+
+
+#endif // !__INTERFACE_H 
