e66583beea3e6b254ec46bfd0cf1c0a5a7d49dab
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-05-03 11:52:45 +0800

    .

---
+++ 03_存档_202505-now/0503_bug_欧拉角计算错误(非常大)/yes/Core/Code/Module/__module.h
@@ -0,0 +1,25 @@
+#ifndef __MODULE_H
+#define __MODULE_H
+
+
+    #include "my_i2c.h"
+    #include "multi_printf.h"
+    #include "my_qmc5883.h"
+    #include "my_mpu9250.h"
+     
+
+    #define LSM_for_1_and_MPU_for_0 0
+
+    
+    #define IMU_Init() {\
+        if (1 == LSM_for_1_and_MPU_for_0){\
+            QMC5883_Init();\
+            /*LSM6DS3_Init();*/\
+        }else{\
+            MPU9250_Init();\
+        }\
+    }
+
+
+#endif // !__MODULE_H    
+
