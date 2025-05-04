d456e1940bd3696bfa24638fbd14f68b3540f2a6
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-05-04 20:01:09 +0800

    .

---
+++ 03_存档_202505-now/0504_⑦_l151_+usart_+flash_+fatfs_+/1.四元数加好了/Core/Code/Tool/my_number.c
@@ -0,0 +1,22 @@
+#include "my_number.h"
+
+
+void reset_array_INT(int *pointer){
+    int length = sizeof(pointer) / pointer[0];
+    for (size_t i = 0; i < length; i++)
+    {
+        pointer[i] = 0;
+    }
+    
+}
+
+void reset_array_FLOAT(float *pointer){
+    int length = sizeof(pointer) / pointer[0];
+    for (size_t i = 0; i < length; i++)
+    {
+        pointer[i] = 0;
+    }
+    
+}
+
+
