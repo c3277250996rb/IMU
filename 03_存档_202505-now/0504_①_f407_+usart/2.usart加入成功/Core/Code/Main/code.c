e331340f3ad2e80021089978deb7a40dad2ba4f6
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2025-05-04 09:58:12 +0800

    .

---
+++ 03_存档_202505-now/0504_①_f407_+usart/2.usart加入成功/Core/Code/Main/code.c
@@ -0,0 +1,41 @@
+/*金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮*/
+#include "code.h"
+
+void CODE_MAIN(void){
+    CODE_INIT();
+    POWER_ON();
+    CODE_SELF_TEST();
+    CODE_WHILE();
+}
+
+
+void CODE_INIT(void){
+	delay_init(di_sysclk);      //初始化延时函数
+    uart_init(115200);          //初始化串口
+    usmart_init(ui_sysclk);     //初始化USMART
+}
+
+void CODE_SELF_TEST(void){
+    while(1){
+        
+        while(1);
+    }
+}
+
+void CODE_WHILE(void){
+
+    while(1){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
